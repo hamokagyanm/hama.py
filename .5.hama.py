03ec0f36a80d37129408791ba997f980b1757bc9
hamokagyanm <57769602+hamokagyanm@users.noreply.github.com>
2021-03-31 12:00:55 +0300

    Create .5.hama.py

---
+++ .5.hama.py
@@ -0,0 +1,19 @@
+import  os, sys , time
+os.system("clear")
+time.sleep(2)
+print("hama")
+while True:
+	h = "hama"
+	j = "hama"
+	jj = input("  Username: ")
+	hh = input("  Passowrd: ")
+	if h == hh and j == jj :
+		print("\033[32m Afarin \033[0;1m:)")
+		print(50*"-")
+		time.sleep(1)
+		os.system("python2 .5.hama")
+	else:
+		print("\033[33;1mHallaya !!")
+		time.sleep(1)
+		os.system("clear")
+		print("hamoka")
